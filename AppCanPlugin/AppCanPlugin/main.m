6fb7f76d9d7b23fe00dc47d2298010d5eb2c4bac
qingjie.song <qingjie.song@zymobi.com>
2018-01-26 17:39:59 +0800

    屏蔽NSLog，main.m增加反动态调试代码

--- AppCanPlugin/AppCanPlugin/main.m
+++ AppCanPlugin/AppCanPlugin/main.m
@@ -10,10 +10,32 @@
 
 #import "WidgetOnePseudoDelegate.h"
 
+#import <dlfcn.h>
+#import <sys/types.h>
+
+void anti_gdb_debug();
+
+typedef int (*ptrace_ptr)(int request,pid_t pid,caddr_t addr,void *data);
+
+#if !defined(PT_DENY_ATTACH)
+#define PT_DENY_ATTACH 31
+#endif
+
 int main(int argc, char *argv[])
 {
+    
+#ifndef  _DEBUG
+    anti_gdb_debug();
+#endif
 
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([WidgetOnePseudoDelegate class]));
     }
+}
+
+void anti_gdb_debug(){
+    void *handle = dlopen(NULL, RTLD_GLOBAL | RTLD_NOW);
+    ptrace_ptr p_ptr = dlsym(handle, "ptrace");
+    p_ptr(PT_DENY_ATTACH,0,0,0);
+    dlclose(handle);
 }
